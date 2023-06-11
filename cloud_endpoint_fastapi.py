56d1f74b1dfe0482ddac289e5d081270144b9f62
Mateusz <greenwaild@gmail.com>
2023-06-11 22:50:13 +0200

    cleanup

--- cloud_endpoint_fastapi.py
+++ cloud_endpoint_fastapi.py
@@ -1,6 +1,3 @@
-# import signal
-# import sys
-
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -15,7 +12,6 @@ class Data(BaseModel):
 @app.post("/your-endpoint")
 def your_endpoint(data: Data):
     value = data.value
-
     # Perform actions based on the numerical value
     if value > 10:
         result = "Value is greater than 10xxx"
@@ -25,17 +21,6 @@ def your_endpoint(data: Data):
     return {"result": result}
 
 
-# # Define a signal handler function
-# def sigterm_handler(signal, frame):
-#     # Perform any necessary cleanup or termination steps here
-#     print("Received SIGTERM signal. Gracefully exiting...")
-#     # Clean up resources, close connections, save data, etc.
-#     sys.exit(0)
-
-
 if __name__ == "__main__":
-    # # Register the signal handler
-    # signal.signal(signal.SIGTERM, sigterm_handler)
-
     # Run the application with uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
